959d2621e451a71912dd091e3e040ea98adf7d60
mattclifford1 <mc15445@my.bristol.ac.uk>
2021-12-01 11:01:07 +0000

    print dataset relevant parts

--- load_data.py
+++ load_data.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset
+import numpy as np
 
 split = "train"
 cache_dir = "./data_cache"
@@ -18,3 +19,23 @@ dataset_document = load_dataset(
     ignore_verifications=True,
     cache_dir=cache_dir,
 )
+
+
+def get_doc_from_id(row=0):
+    turn = 0
+    # print the turn - dict containing info
+    print('First Turn: ', dataset_dialogue['turns'][row][turn], '\n')
+    # print doc_id - is the title of the relevant document
+    id = dataset_dialogue['doc_id'][row]
+    print('Dialogue doc_id: ', id)
+    # confirm is the same as in the document dataset
+    ind = dataset_document['doc_id'].index(id)
+    print('Document doc_id: ', dataset_document['doc_id'][ind], '\n')
+    # print span_id - the span of relevant text in the relevant document (just take the first span for now)
+    span = 0
+    sp_id = dataset_dialogue['turns'][row][turn]['references'][span]['sp_id']
+    print('Document sp_id: ', dataset_document['spans'][ind][int(sp_id)], '\n')
+    print('Document sp_id_title: ', dataset_document['spans'][ind][int(sp_id)]['title'])
+    print('Document sp_id_text: ', dataset_document['spans'][ind][int(sp_id)]['text_sec'])
+
+get_doc_from_id()
