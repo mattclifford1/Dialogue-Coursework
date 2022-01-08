d63220819bf03d603ffa48482ffb0f4684d4e3d1
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-01-08 13:18:23 +0000

    NB clf implemented

--- task1_naive_bayes.py
+++ task1_naive_bayes.py
@@ -2,10 +2,44 @@ import numpy as np
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import BernoulliNB
 import utils
-from word_counts import get_unique_words_in_questions, get_spans_per_doc, count_questions_and_spans
+from word_counts import get_train_val
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score
+
+def train_clf(X_train, y_train):
+    clf = BernoulliNB()
+    clf.fit(X_train, y_train)
+    return clf
 
 
-train_data = utils.load_own_rc_data(split="train")
-val_data = utils.load_own_rc_data(split="validation")
+if __name__ == '__main__':
+    train_data = utils.load_own_rc_data(split="train")
+    val_data = utils.load_own_rc_data(split="validation")
 
-train, val = get_train_val(train_data, val_data)
+    print('Getting word count data')
+    train, val = get_train_val(train_data, val_data)
+
+    # train a binary classifier per span within a context
+    clfs = {}
+    print('Training')
+    for context in tqdm(train.keys()):
+        X = np.array(train[context]['X'])
+        Y = np.array(train[context]['Y'])
+        clfs[context] = {}
+        for span in range(Y.shape[1]):
+            clfs[context][span] = train_clf(X, Y[:, span])
+
+    # validate training
+    scores = []
+    print('Evaluating')
+    for context in tqdm(val.keys()):
+        if val[context]['X'] == []:  # no examples for this context
+            continue
+        X = np.array(val[context]['X'])
+        Y = np.array(val[context]['Y'])
+        for span in range(Y.shape[1]):
+            y_pred = clfs[context][span].predict(X)
+            y_true = Y[:, span]
+            acc = accuracy_score(y_true, y_pred)
+            scores.append(acc)
+    print('AVG score: ', sum(scores)/len(scores)*100, '% out of ', len(scores), ' examples')
